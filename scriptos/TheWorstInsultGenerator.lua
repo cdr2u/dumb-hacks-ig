3cf3e2ed5e7a153137112b7047228ec494bd3d8c
ch-omik <ilikeminecryaft@gmail.com>
2024-12-16 23:26:03 +1000

    Update TheWorstInsultGenerator.lua

--- scriptos/TheWorstInsultGenerator.lua
+++ scriptos/TheWorstInsultGenerator.lua
@@ -1,3 +1,11 @@
-local words = {"floor ", "eyebrow ", "leader ", "product ", "knot ", "enter ", "negotiation ", "interactive ", "possibility ", "switch ", "central ", "agreement ", "trivial ", "lifestyle ", "chain ", "association ", "astonishing ", "economic ", "mark ", "assume ", "drag ", "see ", "trustee ", "fur ", "contempt ", "elephant ", "safe ", "helicopter ", "visible ", "hike ", "buy ", "sand ", "highway ", "rotten ", "tune ", "grind ", "vein ", "medium ", "veteran ", "advantage ", "opposition ", "thoughtful ", "chip ", "spite ", "dentist ", "form ", "cabinet ", "a ", "an ", "at ", "is ", "isn't ", "do", "don't ", "kill ", "destroy ", "yucky ", "robux ", "fortnite ", "roblox ", "shut ", "up ", "down ", "left ", "right ", "quiet ", "opposite ", "the ", "not ", "quite ", "reverse ", "of" , "can ", "call ", "hang ", "yourself ", "you ", "your "}
+local words = {"floor ", "eyebrow ", "leader ", "product ", "knot ", "enter ", "negotiation ", "interactive ", "possibility ", "switch ", "central ", "agreement ", "trivial ", "lifestyle ", "chain ", "association ", "astonishing ", "economic ", "mark ", "assume ", "drag ", "see ", "trustee ", "fur ", "contempt ", "elephant ", "safe ", "helicopter ", "visible ", "hike ", "buy ", "sand ", "highway ", "rotten ", "tune ", "grind ", "vein ", "medium ", "veteran ", "advantage ", "opposition ", "thoughtful ", "chip ", "spite ", "dentist ", "form ", "cabinet ", "a ", "an ", "at ", "is ", "isn't ", "do", "don't ", "kill ", "destroy ", "yucky ", "robux ", "fortnite ", "roblox ", "shut ", "up ", "down ", "left ", "right ", "quiet ", "opposite ", "the ", "not ", "quite ", "reverse ", "of " , "can ", "call ", "hang ", "yourself ", "you ", "your "}
 local targets = {"r dad ", "r mom ", "r brother ", "r sister ", "r house ", "r car "}
 local attacks = {"looks like ", "smells like ", "sounds like ", "feels like ", "is "}
+
+local message = "you" .. targets[math.random(1, #targets)] .. "" .. attacks[math.random(1, #attacks)] .. ""
+local rng = math.random(1, 24)
+
+for i = 0, rng do
+    message = message .. "" .. words[math.random(1, #words)]
+end
+game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(message,"All")
