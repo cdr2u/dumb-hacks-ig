cbd39b6832ef3d28817c874982eaa4578b62fe31
ch-omik <ilikeminecryaft@gmail.com>
2024-12-17 14:34:07 +1000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -3,7 +3,13 @@ local popup = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/
 local ad = popup.CreateLib("cdr2u's Useless Hub", "Ocean")
 local uni = ad:NewTab("Universal")
 local chet = uni:NewSection("Chat Scripts")
+local fournnone = ad:NewTab("4nn1's Place")
+local lulz = fournnone:NewSection("lol")
 
 chet:NewButton("Shitty Roast", "they don't even make sense", function()
   
+end)
+
+lulz:NewButton("Muffy Bomb", "reset to detonate, make sure u are always the closest to muff", function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ch-omik/dumb-hacks-ig/refs/heads/main/scriptos/MuffyBomb.lua"))()
 end)
