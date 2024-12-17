7573a1d6ac893b9b6e747f66a500980e246dac5f
ch-omik <ilikeminecryaft@gmail.com>
2024-12-17 15:19:53 +1000

    Update and rename hlub.lua to hub.lua

--- hub.lua
+++ hub.lua
@@ -7,9 +7,13 @@ local fournnone = ad:NewTab("4nn1's Place")
 local lulz = fournnone:NewSection("lol")
 
 chet:NewButton("Shitty Roast", "they don't even make sense", function()
-  
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ch-omik/dumb-hacks-ig/refs/heads/main/scriptos/TheWorstInsultGenerator.lua"))()
 end)
 
 lulz:NewButton("Muffy Bomb", "reset to detonate, make sure u are always the closest to muff", function()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ch-omik/dumb-hacks-ig/refs/heads/main/scriptos/MuffyBomb.lua"))()
+end)
+
+lulz:NewButton("Socky Bomb", "reset to detonate, make sure u are always the closest to sock", function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ch-omik/dumb-hacks-ig/refs/heads/main/scriptos/SockyBomb.lua"))()
 end)
