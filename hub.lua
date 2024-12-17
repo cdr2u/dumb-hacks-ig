2815adb3da5dd6807283eef3bf9210e0b1076aa4
ch-omik <ilikeminecryaft@gmail.com>
2024-12-17 11:06:04 +1000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,3 +1,4 @@
 local popup = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
 local ad = popup.CreateLib("cdr2u's Useless Hub", "Ocean")
+local chab = ad:NewTab("Chat")
