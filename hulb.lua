785674a8146d61c4152c9bd22e1c9f032e8dcc15
cdr2u <ilikeminecryaft@gmail.com>
2025-03-09 18:34:20 +1000

    Update hulb.lua

--- hulb.lua
+++ hulb.lua
@@ -5,13 +5,15 @@ local uni = ad:NewTab("Universal")
 local chet = uni:NewSection("Chat Scripts")
 local fournnone = ad:NewTab("4nn1's Place")
 local lulz = fournnone:NewSection("lol")
+local chik = ad:NewTab("Find The Chomiks")
+local ball = chik:NewSection("wow")
 
 chet:NewButton("Shitty Roast", "they don't even make sense", function()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ch-omik/dumb-hacks-ig/refs/heads/main/scriptos/TheWorstInsultGenerator.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/cdr2u/dumb-hacks-ig/refs/heads/main/scriptos/TheWorstInsultGenerator.lua"))()
 end)
 
 lulz:NewButton("Muffy Bomb", "reset to detonate, make sure u are always the closest to muff", function()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ch-omik/dumb-hacks-ig/refs/heads/main/scriptos/MuffyBomb.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/cdr2u/dumb-hacks-ig/refs/heads/main/scriptos/MuffyBomb.lua"))()
 end)
 
 lulz:NewButton("Socky Bomb", "reset to detonate, make sure u are always the closest to sock", function()
